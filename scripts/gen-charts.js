e646e0847c81898eded03155f2b8c17c1b2021a6
Simon <simonxd@gmail.com>
2017-11-27 17:49:49 -0200

    graphs

--- scripts/gen-charts.js
+++ scripts/gen-charts.js
@@ -4,11 +4,9 @@ const fs = require('fs')
 const CHART_SPECS = require('./data')
 const pieChart = require('./chart-types/pie')
 
-const CHARTS_DIR = path.join(__dirname, 'output')
+const CHARTS_DIR = path.join(__dirname, '../src/resources/graficos')
 
 CHART_SPECS.forEach((spec) => {
-	// spec.size = 500
-
 	if (spec.type === 'pie') {
 		let svgStr = pieChart(spec)
 
@@ -17,34 +15,3 @@ CHART_SPECS.forEach((spec) => {
 		fs.writeFileSync(path.join(CHARTS_DIR, spec.name + '.svg'), svgStr, 'utf8')
 	}
 })
-
-// console.log(pieChart({
-// 	name: 'riscos-impactos',
-// 	type: 'pie',
-// 	size: 400,
-// 	data: {
-// 		total: 100,
-// 		slices: [
-// 			{
-// 				label: 'Agricultura/agropecuária',
-// 				color: 'red',
-// 				value: 20,
-// 			},
-// 			{
-// 				label: 'Disponibilidade de agua para produção/serviços',
-// 				color: 'pink',
-// 				value: 17,
-// 			},
-// 			{
-// 				label: 'Transporte matéria prima/produtos e serviços',
-// 				color: 'magenta',
-// 				value: 14,
-// 			},
-// 			{
-// 				label: 'Turismo',
-// 				color: 'green',
-// 				value: 11,
-// 			},
-// 		],
-// 	}
-// }))
